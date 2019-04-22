385cfa45057afe9153a7c21739381feed2085bbe
almirakhonsa <almirakhonsa@gmail.com>
2019-04-22 12:08:32 +0700

    RestAPI

---
+++ conn.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "35.240.248.249",
+  user: "kumparanGuest",
+  password: "guestForKumparan",
+  database: "20190422_Almira_Khonsa_Izzaty"
+  // database: "test"
+});
+
+con.connect(function (err){
+    if(err) throw err;
+});
+
+module.exports = con;
